50b471c8e9cd873f51406cb72fd0b6379cdf02aa
shaquillekiragu <skiragu77@gmail.com>
2024-11-27 19:38:20 +0000

    After several hours reworking timer.js to make both the user input and integer authentication functionalities to work, good progress has been made on both

--- timer/timer.js
+++ timer/timer.js
@@ -1,45 +1,42 @@
 const time = document.getElementById("time");
-const hours = document.getElementById("hours");
-const minutes = document.getElementById("minutes");
-const seconds = document.getElementById("seconds");
 const startButton = document.getElementById("startButton");
 const pauseButton = document.getElementById("pauseButton");
 const resetButton = document.getElementById("resetButton");
 
-console.log(
-  hours.value,
-  "<< hours ",
-  minutes.value,
-  "<< minutes ",
-  seconds.value,
-  "<< seconds "
-  //   startButton,
-  //   "<< startButton ",
-  //   pauseButton,
-  //   "<< pauseButton ",
-  //   resetButton,
-  //   "<< resetButton "
-);
+let hours = prompt("Hours input:");
+let minutes = prompt("Minutes input:");
+let seconds = prompt("Seconds input:");
 
 let clock = null;
 
-let hrs = Number(hours.value) || 0;
-let mins = Number(minutes.value) || 0;
-let secs = Number(seconds.value) || 0;
+while (typeof Number(hours) !== "number") {
+  hours = prompt(
+    "Your hours input is not a number, please enter a whole number"
+  );
+}
+while (typeof Number(minutes) !== "number") {
+  minutes = prompt(
+    "Your minutes input is not a number, please enter a whole number"
+  );
+}
+while (typeof Number(seconds) !== "number") {
+  seconds = prompt(
+    "Your seconds input is not a number, please enter a whole number"
+  );
+}
 
-console.log(hrs, "<< hrs", mins, "<< mins", secs, "<< secs");
+let hrs = Number(hours);
+let mins = Number(minutes);
+let secs = Number(seconds);
 
-if (secs >= 60) {
-  secs -= 60;
+while (secs > 59) {
   mins++;
+  secs -= 60;
 }
-
-if (mins >= 60) {
-  mins -= 60;
+while (mins > 59) {
   hrs++;
+  mins -= 60;
 }
-
-console.log(hrs, "<< hrs 2", mins, "<< mins 2", secs, "<< secs 2");
 
 let h = hrs < 10 ? `0${hrs}` : hrs;
 let m = mins < 10 ? `0${mins}` : mins;
@@ -48,38 +45,26 @@ let s = secs < 10 ? `0${secs}` : secs;
 time.innerHTML = `${h}:${m}:${s}`;
 
 function timer() {
-  secs--;
-  if (secs === -1) {
-    secs = 59;
-    mins--;
-  }
-  if (mins === -1) {
-    mins = 59;
-    hrs--;
+  if (hours !== 0 && mins !== 0 && secs !== 0) {
+    secs--;
+    if (secs === -1) {
+      secs = 59;
+      mins--;
+    }
+    if (mins === -1) {
+      mins = 59;
+      hrs--;
+    }
   }
 
-  //   hrs = hrs < 10 ? `0${hrs}` : hrs;
-  //   mins = mins < 10 ? `0${mins}` : mins;
-  //   secs = secs < 10 ? `0${secs}` : secs;
+  let h = hrs < 10 ? `0${hrs}` : hrs;
+  let m = mins < 10 ? `0${mins}` : mins;
+  let s = secs < 10 ? `0${secs}` : secs;
 
   time.innerHTML = `${h}:${m}:${s}`;
 }
 
 function startTimer() {
-  console.log(
-    hours.value,
-    "<< hours 2 ",
-    minutes.value,
-    "<< minutes 2 ",
-    seconds.value,
-    "<< seconds 2 "
-    // startButton,
-    // "<< startButton 2 ",
-    // pauseButton,
-    // "<< pauseButton 2 ",
-    // resetButton,
-    // "<< resetButton 2 "
-  );
   if (clock === null) {
     clock = setInterval(timer, 1000);
     startButton.innerHTML = "Resume";
